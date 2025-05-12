6795e56b807221c40b3498064a20ef23222aa0ae
Femcom-Mari <bongollmaria@gmail.com>
2025-05-12 09:53:03 +0200

    Add existing project files to Git

---
+++ src/main/java/com/ok200/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.ok200.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
