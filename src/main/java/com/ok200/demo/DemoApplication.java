e4921c10548b83ec14345c92146d9dc2c99e3876
Femcom-Mari <bongollmaria@gmail.com>
2025-05-15 15:01:09 +0200

    create a successful data base connection

--- src/main/java/com/ok200/demo/DemoApplication.java
+++ src/main/java/com/ok200/demo/DemoApplication.java
@@ -2,12 +2,21 @@ package com.ok200.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println("Connected Successfully");
 	}
 
 }
